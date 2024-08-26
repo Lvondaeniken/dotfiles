27b74c100932a901881e6895afc77c490afdd65b
Lukas von Daeniken <Lukas.vondaeniken@simdev.ch>
2024-08-26 15:49:25 +0200

    add formatter for python lua and rust

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,7 +1,9 @@
 local options = {
   formatters_by_ft = {
     cpp = {"clang-format"},
-    autopep8 = {"autopep8"},
+    python = {"autopep8"},
+    lua = { "stylua" },
+    rust = { "rustfmt", lsp_format = "fallback" },
   },
 }
 return options
